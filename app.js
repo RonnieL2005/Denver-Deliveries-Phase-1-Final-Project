be950ba4ad333e1effe5040d0f7f389d806c9bb4
Ronnielangat <ronnielangat2020@gmail.com>
2024-01-19 11:05:08 +0300

    first commit

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
-
-// Get the search input and search icon elements
-const searchInput = document.getElementById('searchInput');
-const searchIcon = document.getElementById('searchIcon');
-
-// Add an event listener to the search icon for when it is clicked
-searchIcon.addEventListener('click', function () {
-    // Get the value from the search input
-    const searchTerm = searchInput.value;
-
-    // You can now use the searchTerm for further processing,
-    // for example, you might want to perform a search operation.
-    // For now, let's just log the search term to the console.
-    console.log('Search Term:', searchTerm);
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch data from db.json
+    fetch('db.json')
+        .then(response => response.json())
+        .then(data => {
+            // Do something with the data
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 });
 
- 
+// Search bar 1
+const searchicon1 = document.querySelector('#searchicon1');
+const search1 = document.querySelector('#searchinput1');
 
+searchicon1.addEventListener('click', function () {
+    search1.style.display = 'flex';
+    searchicon1.style.display = 'none';
+});
+
+// Search bar 2
 const searchicon2 = document.querySelector('#searchicon2');
-
-const srchicon2 = document.querySelector('#srchicon2');
-
 const search2 = document.querySelector('#searchinput2');
 
-searchicon2.addEventListener('click',function(){
+searchicon2.addEventListener('click', function () {
     search2.style.display = 'flex';
     searchicon2.style.display = 'none';
-})
+});
 
-
-
-const bar = document.querySelector('.fa-bars');
+// Burger menu
+const barIcon = document.querySelector('#bar-icon');
 const cross = document.querySelector('#hdcross');
 const headerbar = document.querySelector('.headerbar');
 
-bar.addEventListener('click',function(){
-    setTimeout(()=>{
+barIcon.addEventListener('click', function () {
+    setTimeout(() => {
         cross.style.display = 'block';
-    },200);
+    }, 200);
     headerbar.style.right = '0%';
-})
+});
 
-cross.addEventListener('click',function(){
+cross.addEventListener('click', function () {
     cross.style.display = 'none';
     headerbar.style.right = '-100%';
-})
+});
+
 
